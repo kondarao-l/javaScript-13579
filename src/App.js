bf2833f1f537835b2435de459047c957dafa47c3
Durganjaneyulu <you@example.com>
2023-12-11 11:48:13 +0530

    search implemeted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState } from 'react'
+
+
+const cities = [
+  {
+    id:1,
+    name:'Hyd'
+  },
+  {
+    id:2 ,
+    name:'Vizag'
+  },
+  {
+    id:3 ,
+    name:'tirupati'
+  },
+  {
+    id: 4,
+    name:'guntur'
+  },
+  {
+    id: 5,
+    name:'ongole'
+  }
+]
 function App() {
+  
+ 
+  const [data, setData] = useState('')
+  const changeHandler = e => {
+    setData(e.target.value)
+  }
+  const submitHandler = e => {
+    e.preventDefault()
+    setData(data)
+    //console.log(data)
+  }
+  const buttonHnadler = (e) => {
+    e.preventDefault()
+    console.log(data)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h1>Introduction</h1>
+
+        <form onSubmit={submitHandler}>
+          <input type='text' value={data}  onChange={changeHandler} />
+         <input type='submit'/>
+        </form>
+
+        {cities.filter(eachCity=>eachCity.name.toLowerCase().includes(data)).map(eachCity=>(
+        <div key={eachCity.id} onClick={buttonHnadler}>
+       {eachCity.name}
+        </div>
+          ))}
+
+      
+
+      </center>
     </div>
-  );
+  )
 }
 
 export default App;
